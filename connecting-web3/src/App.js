3dc06045e9c8f152fb7acc25119cfb4f9cfc2ae0
AkshitTheComputerScientist <akshit360@live.com>
2022-12-16 15:58:56 +0000

    added connecting wallet functionality

--- connecting-web3/src/App.js
+++ connecting-web3/src/App.js
@@ -1,12 +1,31 @@
 import './App.css';
 import 'bulma/css/bulma.min.css';
 
+import {useState} from 'react';
+
 function App() {
+
+  const [walletAddress, setWalletAddress] = useState("");
 
   async function requestAccount(){
 
     if(window.ethereum){
       console.log('detected')
+
+      try {
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+
+        console.log(accounts)
+        setWalletAddress(accounts)
+        
+      } catch (error) {
+        console.log(error)
+      }
+
+
+
     }
     else{
       console.log('metamask not detected')
@@ -23,6 +42,10 @@ function App() {
       >
         Connect wallet!
       </button>
+
+      <h1> 
+        Wallet Address: {walletAddress}
+      </h1>
 
 
       </header>
